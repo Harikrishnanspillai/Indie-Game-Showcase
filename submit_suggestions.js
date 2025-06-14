b141dc8df87542cd42e953713786e172b67498fe
Harikrishnanspillai <harikrishnansugesh@gmail.com>
2025-06-14 09:53:59 +0530

    Update submit_suggestions.js

--- submit_suggestions.js
+++ submit_suggestions.js
@@ -1,15 +1,7 @@
 document.getElementById("suggestForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const form = this;
-  const formData = new FormData(form);
-
-  fetch("https://ctrl-alt-play.42web.io/submit_suggestion.php", {
-    method: "POST",
-    body: formData
-  }).catch(err => {
-    console.error("Suggestion submission error:", err);
-  });
+  
 
   // Immediately show success message
   document.getElementById("formMessage").textContent =
